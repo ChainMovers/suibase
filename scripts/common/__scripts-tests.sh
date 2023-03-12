82e1018b1f3ff2f67594c849c07c0d8a764f2a3a
mario4tier <mario4tier@users.noreply.github.com>
2023-03-11 23:20:10 -0500

    (#3) Add support for 'cargobin' workdir (was 'cargobinsui').

---
+++ scripts/common/__scripts-tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Script to perform sui-base tests.
+#
+# This is not intended to be called directly by the user.
+#
+# Use --github when called from a "github action" to limit what
+# can be tested in practice on a free tier.
+#
+
+# Parse command-line
+GITHUB_OPTION=false
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        # -t|--target) target="$2"; shift ;; That's an example with a parameter
+        # -f|--flag) flag=1 ;; That's an example flag
+        --github) GITHUB_OPTION=true ;;
+        *)
+        echo "Unknown parameter passed: $1"; exit 1
+        exit 1 ;;
+    esac
+    shift
+done
+
+source ~/sui-base/install
+install_ret_code=$?
+
+if [ "$GITHUB_OPTION" = true ]; then
+  echo "Installation status code = [$install_ret_code]"
+  exit $install_ret_code
+fi
+
+# Eventually add more tests here...
