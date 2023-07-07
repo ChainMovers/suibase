a1a00222e6dcc5ffd572a999323f5ac1c3eda2c8
mario4tier <mario4tier@users.noreply.github.com>
2023-07-07 14:04:23 -0400

    Properly exit retry loop when starting faucet + shell-format whole file

--- scripts/common/__sui-faucet-process.sh
+++ scripts/common/__sui-faucet-process.sh
@@ -10,11 +10,11 @@ start_sui_faucet_process() {
     return
   fi
 
-  exit_if_sui_binary_not_ok;
+  exit_if_sui_binary_not_ok
 
-  update_SUI_FAUCET_PROCESS_PID_var;
+  update_SUI_FAUCET_PROCESS_PID_var
   if [ -n "$SUI_FAUCET_PROCESS_PID" ]; then
-    return;
+    return
   fi
 
   echo "Starting $WORKDIR faucet"
@@ -22,10 +22,10 @@ start_sui_faucet_process() {
   mkdir -p "$CONFIG_DATA_DIR/faucet.wal"
 
   # Try up to 3 times to start the process.
-  end=$((SECONDS+30))
+  end=$((SECONDS + 30))
   ALIVE=false
   AT_LEAST_ONE_SECOND=false
-  for _i in {1..3}; do    
+  for _i in {1..3}; do
     if $SUI_BASE_NET_MOCK; then
       export SUI_FAUCET_PROCESS_PID=$SUI_BASE_NET_MOCK_PID
     else
@@ -38,18 +38,18 @@ start_sui_faucet_process() {
         --port "${CFG_sui_faucet_port:?}" \
         --request-buffer-size "${CFG_sui_faucet_request_buffer_size:?}" \
         --wallet-client-timeout-secs "${CFG_sui_faucet_client_timeout_secs:?}" \
-        --write-ahead-log "$CONFIG_DATA_DIR/faucet.wal" >& "$CONFIG_DATA_DIR/sui-faucet-process.log" &
+        --write-ahead-log "$CONFIG_DATA_DIR/faucet.wal" >&"$CONFIG_DATA_DIR/sui-faucet-process.log" &
     fi
 
-    # Loop until confirms can connect, or exit if that takes more than 20 seconds.
+    # Loop until confirms can connect, or exit if takes too much time.
     while [ $SECONDS -lt $end ]; do
       # If it returns an error about "JSON" then it is alive!
       if $SUI_BASE_NET_MOCK; then
         CHECK_ALIVE="imagine some JSON here"
       else
         CHECK_ALIVE=$(curl -x "" -s --location \
-                --request POST "http://${CFG_sui_faucet_host_ip:?}:${CFG_sui_faucet_port:?}/gas" \
-                --header 'Content-Type: application/json' --data-raw '{Bad Request}' | grep "JSON")
+          --request POST "http://${CFG_sui_faucet_host_ip:?}:${CFG_sui_faucet_port:?}/gas" \
+          --header 'Content-Type: application/json' --data-raw '{Bad Request}' | grep "JSON")
       fi
       if [ -n "$CHECK_ALIVE" ]; then
         ALIVE=true
@@ -71,6 +71,11 @@ start_sui_faucet_process() {
         break
       fi
     done
+
+    # If it is alive, then break the retry loop.
+    if [ "$ALIVE" = true ]; then
+      break
+    fi
   done
 
   # Just UI aesthetic newline for when there was "." printed.
@@ -78,13 +83,13 @@ start_sui_faucet_process() {
     echo
   fi
 
-  # Act on success/failure of the sui process responding to "sui client".
+  # Act on success/failure of the process responding.
   if [ "$ALIVE" = false ]; then
     echo "sui-faucet process not responding. Try again? (may be the host is too slow?)."
-    exit;
+    exit
   fi
 
-  update_SUI_FAUCET_PROCESS_PID_var;
+  update_SUI_FAUCET_PROCESS_PID_var
   echo "faucet started (process pid $SUI_FAUCET_PROCESS_PID)"
 }
 export -f start_sui_faucet_process
@@ -92,7 +97,7 @@ export -f start_sui_faucet_process
 stop_sui_faucet_process() {
   # success/failure is reflected by the SUI_PROCESS_PID var.
   # noop if the process is already stopped.
-  update_SUI_FAUCET_PROCESS_PID_var;
+  update_SUI_FAUCET_PROCESS_PID_var
   if [ -n "$SUI_FAUCET_PROCESS_PID" ]; then
     echo "Stopping faucet (process pid $SUI_FAUCET_PROCESS_PID)"
 
@@ -103,10 +108,10 @@ stop_sui_faucet_process() {
     fi
 
     # Make sure it is dead.
-    end=$((SECONDS+15))
+    end=$((SECONDS + 15))
     AT_LEAST_ONE_SECOND=false
     while [ $SECONDS -lt $end ]; do
-      update_SUI_FAUCET_PROCESS_PID_var;
+      update_SUI_FAUCET_PROCESS_PID_var
       if [ -z "$SUI_FAUCET_PROCESS_PID" ]; then
         break
       else
@@ -138,7 +143,7 @@ update_SUI_FAUCET_PROCESS_PID_var() {
   if $SUI_BASE_NET_MOCK; then return; fi
 
   local _PID
-  _PID=$(get_process_pid sui-faucet);
+  _PID=$(get_process_pid sui-faucet)
   if [ "$_PID" = "NULL" ]; then
     unset SUI_FAUCET_PROCESS_PID
   else
@@ -157,30 +162,33 @@ faucet_command() {
 
   for option in $1; do
     case $option in
-    -j|--json)
+    -j | --json)
       if $_OPT_JSON; then setup_error "error: Option '$option' specified more than once"; fi
-      _OPT_JSON=true ;;
+      _OPT_JSON=true
+      ;;
     *)
       # It has to be either one or more "0x" or a single "all"
       local _ADDR
-      _ADDR=$( echo "$option" | tr '[:upper:]' '[:lower:]' | tr -d "[:blank:]" )
+      _ADDR=$(echo "$option" | tr '[:upper:]' '[:lower:]' | tr -d "[:blank:]")
       case $_ADDR in
-      all|-all|--all)
-        if (( ${#_ALL_ADDRS[@]} )); then setup_error "error: Can't mix option 'all' with enumerating address"; fi
+      all | -all | --all)
+        if ((${#_ALL_ADDRS[@]})); then setup_error "error: Can't mix option 'all' with enumerating address"; fi
         if $_OPT_ALL; then setup_error "error: Option '$_ADDR' specified more than once"; fi
-        _OPT_ALL=true ;;
+        _OPT_ALL=true
+        ;;
       0x*)
         if $_OPT_ALL; then setup_error "error: Can't mix option 'all' with enumerating address"; fi
-        exit_if_not_valid_sui_address "$_ADDR";
-        _ALL_ADDRS+=( "$_ADDR" )
-      ;;
+        exit_if_not_valid_sui_address "$_ADDR"
+        _ALL_ADDRS+=("$_ADDR")
+        ;;
       *) setup_error "Invalid hexadecimal address [$_ADDR]" ;;
-      esac ;; # address field parsing
+      esac
+      ;; # address field parsing
     esac # outer options parsing
   done
 
   if [ $_OPT_ALL = false ] && [ ${#_ALL_ADDRS[@]} -eq 0 ]; then
-    cd_sui_log_dir;
+    cd_sui_log_dir
     echo "http://${CFG_sui_faucet_host_ip:?}:${CFG_sui_faucet_port:?}"
     local _FAUCET_ADDR
     _FAUCET_ADDR=$($SUI_BIN_ENV "$SUI_BIN_DIR/sui" client --client.config "$WORKDIRS/$WORKDIR/faucet/client.yaml" active-address)
@@ -203,10 +211,9 @@ faucet_command() {
     local _RESP
     _RESP=$($SUI_EXEC client addresses | grep "0x" | awk '{print $1}')
 
-    while IFS= read -r line
-    do
-      exit_if_not_valid_sui_address "$line";
-     _ALL_ADDRS+=( "$line" )
+    while IFS= read -r line; do
+      exit_if_not_valid_sui_address "$line"
+      _ALL_ADDRS+=("$line")
     done < <(printf '%s\n' "$_RESP")
 
     echo "${#_ALL_ADDRS[@]} addresses found."
@@ -220,24 +227,24 @@ faucet_command() {
     local _RESP
 
     _RESP=$(curl -x "" -s --location \
-    --request POST "http://${CFG_sui_faucet_host_ip:?}:${CFG_sui_faucet_port:?}/gas" \
-    --header "Content-Type: application/json" \
-    --data-raw "{ \"FixedAmountRequest\": {\"recipient\": \"$_addr\"}}")
+      --request POST "http://${CFG_sui_faucet_host_ip:?}:${CFG_sui_faucet_port:?}/gas" \
+      --header "Content-Type: application/json" \
+      --data-raw "{ \"FixedAmountRequest\": {\"recipient\": \"$_addr\"}}")
 
     _RESP_CLEAN=$(echo "$_RESP" | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]' | tr -d '_')
     # Check for 3 confirmations of success.
     local _ERROR_ID
-    (( _ERROR_ID=0 ))
+    ((_ERROR_ID = 0))
     if [[ "$_RESP_CLEAN" != *"transferredgasobjects"* ]]; then
-      (( _ERROR_ID += 1 ))
+      ((_ERROR_ID += 1))
     fi
 
     if [[ "$_RESP_CLEAN" != *"transfertxdigest"* ]]; then
-      (( _ERROR_ID += 10 ))
+      ((_ERROR_ID += 10))
     fi
 
     if [[ "$_RESP_CLEAN" != *"\"error\":null"* ]]; then
-      (( _ERROR_ID += 100 ))
+      ((_ERROR_ID += 100))
     fi
 
     _N_COINS=$(count_coins "$_RESP_CLEAN")
@@ -261,18 +268,17 @@ count_coins() {
   local _SEP="amount"
   local _COUNT
 
-  (( _COUNT=0 ))
+  ((_COUNT = 0))
   _TMP=${_STR//"$_SEP"/$'\2'}
-  IFS=$'\2' read -r -a arr <<< "$_TMP"
-  for _substr in "${arr[@]}" ; do
+  IFS=$'\2' read -r -a arr <<<"$_TMP"
+  for _substr in "${arr[@]}"; do
     #echo "<$_substr>"
-    (( ++_COUNT ))
+    ((++_COUNT))
   done
   unset IFS
   if [ $_COUNT -ge 2 ]; then
-   (( --_COUNT ))
+    ((--_COUNT))
   fi
 
   echo "$_COUNT"
 }
-
