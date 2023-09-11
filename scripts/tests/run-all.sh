98fa3f17e25c00e9bd7bd3e6222b173bc9126348
mario4tier <mario4tier@users.noreply.github.com>
2023-09-10 20:14:00 -0400

    Fix to allow calling automated tests from any directory

--- scripts/tests/run-all.sh
+++ scripts/tests/run-all.sh
@@ -15,28 +15,32 @@
 #    should not be deployed.
 
 main() {
-    local _AT_LEAST_ONE_FATAL_ERROR_FOUND=false
-    TEST_SCRIPTS=$(find . -name "*.sh" -type f -not -path "./run-all.sh" -print0 | sort -z | tr '\0' ' ')
-    for SCRIPT in $TEST_SCRIPTS; do
-        echo "Running $SCRIPT..."
-        bash "$SCRIPT"
-        local _CODE=$?
-        if [[ $_CODE -ne 0 ]]; then
-            _AT_LEAST_ONE_FATAL_ERROR_FOUND=true
-            echo "Fatal error found in $SCRIPT. Exit code=$_CODE"
-            if [[ $_CODE -eq 1 ]]; then
-                break
-            fi
-        fi
-    done
-
-    if [ "$_AT_LEAST_ONE_FATAL_ERROR_FOUND" = "true" ]; then
-        echo "Fatal error found. Code should not be deployed."
-        exit 1
-    else
-        echo "All tests passed."
-        exit 0
+  local _AT_LEAST_ONE_FATAL_ERROR_FOUND=false
+  TEST_SCRIPTS=$(find ~/suibase/scripts/tests/. -name "*.sh" -type f -print0 | sort -z | tr '\0' ' ')
+  for SCRIPT in $TEST_SCRIPTS; do
+    # Skip run-all.sh!
+    if [[ "$SCRIPT" == *"run-all.sh" ]]; then
+      continue
     fi
+    echo "Running $SCRIPT..."
+    bash "$SCRIPT"
+    local _CODE=$?
+    if [[ $_CODE -ne 0 ]]; then
+      _AT_LEAST_ONE_FATAL_ERROR_FOUND=true
+      echo "Fatal error found in $SCRIPT. Exit code=$_CODE"
+      if [[ $_CODE -eq 1 ]]; then
+        break
+      fi
+    fi
+  done
+
+  if [ "$_AT_LEAST_ONE_FATAL_ERROR_FOUND" = "true" ]; then
+    echo "Fatal error found. Code should not be deployed."
+    exit 1
+  else
+    echo "All tests passed."
+    exit 0
+  fi
 }
 
 main
